309a9d79c26b43a5627211b7ae7bede8feeefbb9
Michael Grubb <michael@grubb.us>
2015-12-15 21:02:12 -0600

    Day 1 Part 1 Solution

--- src/advent/day1.clj
+++ src/advent/day1.clj
@@ -1,1 +1,20 @@
-(ns advent.day1)
+(ns advent.day1
+  (:require [clojure.java.io :as io]))
+
+(defn floor
+  "Counts parens to go up or down"
+  [s]
+  (letfn
+    [(do-char [c s]
+      (condp = c
+        \( (inc s)
+        \) (dec s)
+        s))]
+    (loop [[h & t] s sum 0]
+      (if h
+        (recur t (do-char h sum))
+        sum))))
+
+(defn solution
+  []
+  (floor (slurp (io/resource "day1-input.txt"))))
--- test/advent/day1_test.clj
+++ test/advent/day1_test.clj
@@ -2,4 +2,28 @@
   (:require [clojure.test :refer :all]
             [advent.day1 :refer :all]))
 
+(deftest floor-0
+  (is (= (floor "") 0))
+  (is (= (floor nil) 0))
+  (is (= (floor "(())") 0))
+  (is (= (floor "()()") 0)))
+
+(deftest floor-3
+  (is (= (floor "(((") 3))
+  (is (= (floor "(()(()(")))
+  (is (= (floor "))((((("))))
+
+(deftest floor--1
+  (is (= (floor "())") -1))
+  (is (= (floor "))(") -1)))
+
+(deftest floor--3
+  (is (= (floor ")))") -3))
+  (is (= (floor ")())())"))))
+
+(deftest floor-invalidchar
+  (is (= (floor ") ) )") -3))
+  (is (= (floor "(a)b)") -1)))
+
+(println "Solution for day1 part 1 is:" (solution))
 
