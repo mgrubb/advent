b6869d3d42f144c93ae94ee4bff4835d9df86197
Michael Grubb <michael@grubb.us>
2015-12-31 01:01:15 -0600

    Run solutions from -main

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,8 +1,22 @@
 (ns advent.core
-  (:require [advent.day1 :as d1]))
+  (:require [advent
+             [day1 :as d1]
+             [day2 :as d2]
+             [day3 :as d3]
+             [day4 :as d4]
+             [day5 :as d5]
+             [day6 :as d6]
+             [day7 :as d7]
+             [day8 :as d8]]))
 
 (defn -main
   [& args]
-  (println "Day 1")
-  (println "  Part 1:" (d1/solution))
-  (println "  Part 2:" (d1/solution-2)))
+  (d1/solution)
+  (d2/solution)
+  (d3/solution)
+  (d3/solution)
+  ;(d4/solution)
+  (d5/solution)
+  ;(d6/solution)
+  (d7/solution)
+  (d8/solution))
