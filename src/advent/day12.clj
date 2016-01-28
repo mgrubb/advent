857c4af0d6f9c7fb78ee994b57000918398d349c
Michael Grubb <michael@grubb.us>
2016-01-27 18:23:02 -0600

    Day 12 Solution 1

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -1,4 +1,24 @@
 (ns advent.day12
-  (:require [advent.solution :refer [defsolution]]))
+  (:require [clojure.java.io :as io]
+            [clojure.walk :refer [postwalk]]
+            [clojure.data.json :as json]
+            [advent.solution :refer [defsolution]]))
 
+(defn read-input
+  [f]
+  (-> f
+      io/resource
+      io/reader
+      (json/read :key-fn keyword)))
+
+(defn sum-walk
+  [d]
+  (let [res (volatile! 0)]
+    (postwalk #(if (number? %)
+                 (vswap! res + %)
+                 %) d)
+    @res))
+
+(defsolution 12
+  (show 1 (sum-walk (read-input "day12-input.txt"))))
 
--- test/advent/day12_test.clj
+++ test/advent/day12_test.clj
@@ -1,3 +1,10 @@
 (ns advent.day12-test
   (:require [clojure.test :refer :all]
             [advent.day12 :refer :all]))
+
+
+(deftest sum-walk-test
+  (is (= (+ 12 13 1 2 3 4 5 6 7 8 9)
+         (sum-walk [{:a 12 :b {:c 13} :d [1 2 3]} [4 5 6] [7 {:e 8 :f [9]}]]))))
+
+(solution)
