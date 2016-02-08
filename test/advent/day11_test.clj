031bb9117f72c7cbfa1f4e860e323b91951716bb
Michael Grubb <michael@grubb.us>
2016-02-08 11:39:24 -0600

    account for change in day11 code

--- test/advent/day11_test.clj
+++ test/advent/day11_test.clj
@@ -2,39 +2,32 @@
   (:require [clojure.test :refer :all]
             [advent.day11 :refer :all]))
 
-(deftest inc-str-test
-  (are [e x] (= e (inc-str x))
-       "b" \a
-       "c" "b"
-       "ba" "z"
-       "ya" "xz"))
-
 (deftest no-bad-chars-test
-  (are [e x] (= e (no-bad-chars? x))
+  (are [e x] (= e (no-bad-chars? (str->nums x)))
        true "abcdefgh"
        false "abcdefghi"
        false "jklmn"
        false "mnopqrs"))
 
 (deftest has-straight-test
-  (are [e x] (= e (has-straight? x))
+  (are [e x] (= e (has-straight? (str->nums x)))
        false "abd"
        false "abdegf"
        true "abcdef"))
 
 (deftest has-pairs-test
-  (are [e x] (= e (has-pairs? x))
+  (are [e x] (= e (has-pairs? (str->nums x)))
        false "aabcdef"
        false "abcddefg"
        true "aabb"
        true "aabccdee"))
 
 (deftest valid-password-test
-  (are [e x] (= e (valid-password? x))
+  (are [e x] (= e (valid-password? (str->nums x)))
        false "hijaabccdef"
        true "hjaabccdeff"))
 
 (deftest find-next-password-test
   (is (= "aabcce" (find-next-password "aabccd"))))
 
-(solution)
+(solution false)
